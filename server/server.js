00002ece5e8a46073dac4596b1c20906a88c8970
Dave <bbec0689@gmail.com>
2021-04-12 09:21:50 -0600

    updated file structure and users

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const routes = require('./routes');
 
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,9 +19,6 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -30,4 +28,5 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
