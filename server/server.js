9f003d28dbeae18aeb3667b1e8512397d7544391
Dave <bbec0689@gmail.com>
2021-04-12 17:28:51 -0600

    heroku push

--- server/server.js
+++ server/server.js
@@ -32,3 +32,5 @@ db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+
+//to commit
